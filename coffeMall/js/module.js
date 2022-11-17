548395bb4f689323f9c812a23090802e718f2a8c
Whiffe <33534050+Whiffe@users.noreply.github.com>
2022-11-17 23:56:17 +0800

    Add files via upload

---
+++ coffeMall/js/module.js
@@ -0,0 +1,6 @@
+ 
+ /*打开注册窗口*/
+   function reg(){    window.open("register.html","mywin","top=150,left=300,width=400,height=300,menubar=no");   } 
+/*打开登录窗口*/
+   function login(){   window.open("login.html","mywin","top=150,left=300,width=400,height=300,menubar=no");   } 
+ 
